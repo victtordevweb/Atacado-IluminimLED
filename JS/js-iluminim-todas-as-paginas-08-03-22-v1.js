94e3dc2022eef214a9cf9383e41be967982c9bca
victtordevweb <victorsilvabstronger@gmail.com>
2022-03-08 09:41:07 -0300

    Icon representante

--- JS/js-iluminim-todas-as-paginas-08-03-22-v1.js
+++ JS/js-iluminim-todas-as-paginas-08-03-22-v1.js
@@ -897,6 +897,20 @@ var TodasAsPaginas = {
                      </div>
                   </div>
 
+                  <div class="conteudo-superior-item item-superior-representante-comercial">
+                     <div class="conteudo-superior-titulo">
+                           <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="briefcase" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-briefcase fa-w-16"><path fill="currentColor" d="M464 128H352V56c0-13.26-10.74-24-24-24H184c-13.26 0-24 10.74-24 24v72H48c-26.51 0-48 21.49-48 48v256c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V176c0-26.51-21.49-48-48-48zM192 64h128v64H192V64zm288 368c0 8.82-7.18 16-16 16H48c-8.82 0-16-7.18-16-16V288h160v40c0 13.25 10.75 24 24 24h80c13.25 0 24-10.75 24-24v-40h160v144zM224 320v-32h64v32h-64zm256-64H32v-80c0-8.82 7.18-16 16-16h416c8.82 0 16 7.18 16 16v80z" class=""></path></svg>
+                           <a href="/pagina/seja-nosso-representante-comercial.html" class="atendimento__texto">Representante comercial</a>
+                     </div>
+
+                     <div class="dropdown-conteudo-superior dropdown-superior-padrao">
+                           <div class="conteudo-dropdown">
+                              <span>Seja nosso representante comercial</span>
+                              <span>Acesse e saiba mais!</span>
+                           </div>
+                     </div>
+                  </div>
+
                </div>
 
                <div class="conteudo-superior-2">
